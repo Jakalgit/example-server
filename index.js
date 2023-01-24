3c1482bfb9ebaaf3a2cbc22db464df6f8d17594a
Jakalgit <mistetsmit05@gmail.com>
2023-01-24 16:23:16 +0300

    comm

--- index.js
+++ index.js
@@ -13,14 +13,9 @@ const bodyParser = require("body-parser");
 const PORT = 5000
 
 const app = express()
-app.use(cors())
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-})
+app.use(cors({
+    origin: 'https://workrc.ru/catalog/'
+}))
 app.use(bodyParser.urlencoded({
     extended: true
 }));
