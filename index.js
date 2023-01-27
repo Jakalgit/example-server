cd9e532ac4f2eb29893168cb6cd730f79edd2658
Jakalgit <mistetsmit05@gmail.com>
2023-01-27 14:08:08 +0300

    comm

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = 5000
 const app = express()
 app.options('*', cors())
 app.use(cors({
-    origin: '*'
+    origin: 'https://workrc.ru'
 }))
 app.use(bodyParser.urlencoded({
     extended: true
